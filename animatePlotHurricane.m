54d576ef6885c1c8afeda48aef5bb6e202c4429a
bmroylan <ben.m.roylance@gmail.com>
2026-04-17 15:53:51 -0400

    working and passed the graders

--- animatePlotHurricane.m
+++ animatePlotHurricane.m
@@ -14,6 +14,32 @@ function animatePlotHurricane(hurricaneData)
 %         script hurricaneMapper.m using createFigureWithBackground.m
 
 
+%figure out the length of the data array for use in the for loop
+numPoints=length(hurricaneData.lat);
+
+%initiate the for loop to iterate through from 1 to the end of the length
+%of the array.
+    for i=1:numPoints 
+        currentLat=hurricaneData.lat(i); %gets latitude for current iteration
+        currentLon=hurricaneData.lon(i); %gets longitude for current iteration
+        currentWind=hurricaneData.wind(i); %gets wind speed for current iteration
+
+        %use the calc category function to determine the color based off
+        %the category of the hurricane (using the current wind speed)
+        [~,dotColor]=calcCategory(currentWind);
+        
+        %plot the information for this iteration as a dot, with marker size
+        %15 and the color based off of what we found earlier.
+        plot(currentLon, currentLat, '.', 'MarkerSize',15,'Color', dotColor);
+
+        %pause after the dot is plotted
+        pause(0.01);
+
+        %force the graph to update
+        drawnow;
+
+
+    end
 
 
 
